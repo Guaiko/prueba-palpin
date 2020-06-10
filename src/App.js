326f2b9a9e3fc211c991c5f968098cf73b0c1067
Guaiko <66497615+Guaiko@users.noreply.github.com>
2020-06-09 20:50:28 -0400

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+//#Components
+import Formulary from './components/Formulary';
+
+function App() {
+  return (
+    <div className="App">
+      <Formulary />
+    </div>
+  );
+}
+
+export default App;
